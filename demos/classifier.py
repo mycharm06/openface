30858878c57cab3c71a4750ab701dc5da3e424c4
mycharm06 <arif.del95@gmail.com>
2018-03-14 16:35:35 +0530

    Added connection with database

--- demos/classifier.py
+++ demos/classifier.py
@@ -17,10 +17,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import gzip
 import time
-print 'hello'
 from flask import Flask
+from flask import jsonify
 
 start = time.time()
 
@@ -37,7 +36,7 @@ np.set_printoptions(precision=2)
 import pandas as pd
 
 import openface
-
+import MySQLdb
 from sklearn.pipeline import Pipeline
 from sklearn.lda import LDA
 from sklearn.preprocessing import LabelEncoder
@@ -52,12 +51,17 @@ modelDir = os.path.join(fileDir, '..', 'models')
 dlibModelDir = os.path.join(modelDir, 'dlib')
 openfaceModelDir = os.path.join(modelDir, 'openface')
 
+db = MySQLdb.connect("localhost","root","qwerty","user" )
+
 def getRep(imgPath, multiple=False):
+    imgPath1 = os.path.abspath(imgPath)
     start = time.time()
-    bgrImg = cv2.imread(imgPath)
+    print imgPath1
+    bgrImg = cv2.imread(imgPath1)
     print bgrImg
     if bgrImg is None:
-        raise Exception("Unable to load image: {}".format(imgPath))
+        return None
+        # raise Exception("Unable to load image: {}".format(imgPath))
 
     rgbImg = cv2.cvtColor(bgrImg, cv2.COLOR_BGR2RGB)
 
@@ -94,7 +98,6 @@ def getRep(imgPath, multiple=False):
         #     print("This bbox is centered at {}, {}".format(bb.center().x, bb.center().y))
 
         start = time.time()
-        #give path to your nn4.small2.v1.t7
         net = openface.TorchNeuralNet("/home/ankush/Desktop/openFace/openface/models/openface/nn4.small2.v1.t7", imgDim=96,
                                   cuda=False)
         rep = net.forward(alignedFace)
@@ -108,79 +111,82 @@ def getRep(imgPath, multiple=False):
 
 def train(args):
     print("Loading embeddings.")
-    fname = "{}/labels.csv".format("/home/ankush/Desktop/openFace/openface/generated-embeddings")
+    fname = "{}/labels.csv".format("/home/ankush/openface/generated-embeddings")
     labels = pd.read_csv(fname, header=None).as_matrix()[:, 1]
     labels = map(itemgetter(1),
                  map(os.path.split,
                      map(os.path.dirname, labels)))  # Get the directory.
-    fname = "{}/reps.csv".format("/home/ankush/Desktop/openFace/openface/generated-embeddings")
+    fname = "{}/reps.csv".format("/home/ankush/openface/generated-embeddings")
     embeddings = pd.read_csv(fname, header=None).as_matrix()
     le = LabelEncoder().fit(labels)
     labelsNum = le.transform(labels)
     nClasses = len(le.classes_)
     print("Training for {} classes.".format(nClasses))
 
-    if args.classifier == 'LinearSvm':
-        clf = SVC(C=1, kernel='linear', probability=True)
-    elif args.classifier == 'GridSearchSvm':
-        print("""
-        Warning: In our experiences, using a grid search over SVM hyper-parameters only
-        gives marginally better performance than a linear SVM with C=1 and
-        is not worth the extra computations of performing a grid search.
-        """)
-        param_grid = [
-            {'C': [1, 10, 100, 1000],
-             'kernel': ['linear']},
-            {'C': [1, 10, 100, 1000],
-             'gamma': [0.001, 0.0001],
-             'kernel': ['rbf']}
-        ]
-        clf = GridSearchCV(SVC(C=1, probability=True), param_grid, cv=5)
-    elif args.classifier == 'GMM':  # Doesn't work best
-        clf = GMM(n_components=nClasses)
+    # trainParser.classifier == 'LinearSvm'
+    clf = SVC(C=1, kernel='linear', probability=True)
+    # elif args.classifier == 'GridSearchSvm':
+    #     print("""
+    #     Warning: In our experiences, using a grid search over SVM hyper-parameters only
+    #     gives marginally better performance than a linear SVM with C=1 and
+    #     is not worth the extra computations of performing a grid search.
+    #     """)
+    #     param_grid = [
+    #         {'C': [1, 10, 100, 1000],
+    #          'kernel': ['linear']},
+    #         {'C': [1, 10, 100, 1000],
+    #          'gamma': [0.001, 0.0001],
+    #          'kernel': ['rbf']}
+    #     ]
+    #     clf = GridSearchCV(SVC(C=1, probability=True), param_grid, cv=5)
+    # elif args.classifier == 'GMM':  # Doesn't work best
+    #     clf = GMM(n_components=nClasses)
 
-    # ref:
-    # http://scikit-learn.org/stable/auto_examples/classification/plot_classifier_comparison.html#example-classification-plot-classifier-comparison-py
-    elif args.classifier == 'RadialSvm':  # Radial Basis Function kernel
-        # works better with C = 1 and gamma = 2
-        clf = SVC(C=1, kernel='rbf', probability=True, gamma=2)
-    elif args.classifier == 'DecisionTree':  # Doesn't work best
-        clf = DecisionTreeClassifier(max_depth=20)
-    elif args.classifier == 'GaussianNB':
-        clf = GaussianNB()
+    # # ref:
+    # # http://scikit-learn.org/stable/auto_examples/classification/plot_classifier_comparison.html#example-classification-plot-classifier-comparison-py
+    # elif args.classifier == 'RadialSvm':  # Radial Basis Function kernel
+    #     # works better with C = 1 and gamma = 2
+    #     clf = SVC(C=1, kernel='rbf', probability=True, gamma=2)
+    # elif args.classifier == 'DecisionTree':  # Doesn't work best
+    #     clf = DecisionTreeClassifier(max_depth=20)
+    # elif args.classifier == 'GaussianNB':
+    #     clf = GaussianNB()
 
-    # ref: https://jessesw.com/Deep-Learning/
-    elif args.classifier == 'DBN':
-        from nolearn.dbn import DBN
-        clf = DBN([embeddings.shape[1], 500, labelsNum[-1:][0] + 1],  # i/p nodes, hidden nodes, o/p nodes
-                  learn_rates=0.3,
-                  # Smaller steps mean a possibly more accurate result, but the
-                  # training will take longer
-                  learn_rate_decays=0.9,
-                  # a factor the initial learning rate will be multiplied by
-                  # after each iteration of the training
-                  epochs=300,  # no of iternation
-                  # dropouts = 0.25, # Express the percentage of nodes that
-                  # will be randomly dropped as a decimal.
-                  verbose=1)
+    # # ref: https://jessesw.com/Deep-Learning/
+    # elif args.classifier == 'DBN':
+    #     from nolearn.dbn import DBN
+    #     clf = DBN([embeddings.shape[1], 500, labelsNum[-1:][0] + 1],  # i/p nodes, hidden nodes, o/p nodes
+    #               learn_rates=0.3,
+    #               # Smaller steps mean a possibly more accurate result, but the
+    #               # training will take longer
+    #               learn_rate_decays=0.9,
+    #               # a factor the initial learning rate will be multiplied by
+    #               # after each iteration of the training
+    #               epochs=300,  # no of iternation
+    #               # dropouts = 0.25, # Express the percentage of nodes that
+    #               # will be randomly dropped as a decimal.
+    #               verbose=1)
 
-    if args.ldaDim > 0:
-        clf_final = clf
-        clf = Pipeline([('lda', LDA(n_components=args.ldaDim)),
-                        ('clf', clf_final)])
+    # if args.ldaDim > 0:
+    clf_final = clf
+    clf = Pipeline([('lda', LDA(n_components=1)),
+                    ('clf', clf_final)])
 
     clf.fit(embeddings, labelsNum)
 
-    fName = "{}/classifier.pkl".format("/home/ankush/Desktop/openFace/openface/generated-embeddings")
+    fName = "{}/classifier.pkl".format("/home/ankush/openface/generated-embeddings")
     print("Saving classifier to '{}'".format(fName))
     with open(fName, 'w') as f:
         pickle.dump((le, clf), f)
+    flag="Training Complete"
+    return ({'Status' : flag})
 
 
 def infer(args, multiple=False):
     print args
-    #give path to your classifier.pkl file
-    with open("/home/ankush/Desktop/openFace/openface/generated-embeddings/classifier.pkl", 'rb') as f:
+    imagepath=os.path.abspath(args)
+    print imagepath
+    with open("/home/ankush/openface/generated-embeddings/classifier.pkl", 'rb') as f:
         if sys.version_info[0] < 3:
                 (le, clf) = pickle.load(f)
         else:
@@ -189,6 +195,9 @@ def infer(args, multiple=False):
     for img in args:
         print("\n=== {} ===".format(args))
         reps = getRep(args, multiple)
+        if reps is None:
+            error="Unable to load image"
+            return ({'Error' : error})
         if len(reps) > 1:
             print("List of faces in image from left to right")
         for r in reps:
@@ -198,27 +207,34 @@ def infer(args, multiple=False):
             predictions = clf.predict_proba(rep).ravel()
             maxI = np.argmax(predictions)
             person = le.inverse_transform(maxI)
+            cursor = db.cursor()
+            sql1 = "SELECT user_name FROM data WHERE user_id = ('%s')" % (person)
+            cursor.execute(sql1)
+            results = cursor.fetchall()
+            for row in results:
+                ids = row[0]
+                print ids
             confidence = predictions[maxI]
+            print person
             # if args.verbose:
             #     print("Prediction took {} seconds.".format(time.time() - start))
-            print multiple
             if multiple:
 
                 print("Predict {} @ x={} with {:.2f} confidence.".format(person.decode('utf-8'), bbx,
                                                                          confidence))
             else:
-                #checking the confidence condition
-                if confidence <= 0.60:
+                if confidence <= 0.80:
                     print("Wrong face found")
                     print confidence
-                    wrgface="wrong face found"
-                    return wrgface
+                    wrongface="wrong face found"
+                    return ({'please try again' : wrongface})
                 else :
                     confidence=confidence*100
-                    pre = "Welcome {}".format(person.decode('utf-8'))
+                    pre = "Welcome {}".format(ids)
                     print pre
-                    print("Predict {} with {:.2f} confidence.".format(person.decode('utf-8'), confidence))
-                    return pre
+                    print("Predict {} with {:.2f} confidence.".format(ids, confidence))
+                    return ({'name' : pre , 'Confidence' : confidence})
+                    # return jsonify({'name': pokemon_name, 'description': pokemon_desc, "msg": msg})
 
 
             if isinstance(clf, GMM):
@@ -267,7 +283,7 @@ if __name__ == '__main__':
         help='The type of classifier to use.',
         default='LinearSvm')
     trainParser.add_argument(
-        "/home/ankush/Desktop/openFace/openface/generated-embeddings",
+        '--workDir',default='/home/ankush/openface/generated-embeddings',
         type=str,
         help="The input work directory containing 'reps.csv' and 'labels.csv'. Obtained from aligning a directory with 'align-dlib' and getting the representations with 'batch-represent'.")
 
